f688e68086a582b0f4f25d9269e8bb52b52e3f45
Yash Saxena <yashsaxena003@gmail.com>
2025-01-31 00:20:18 +0530

    updated

--- main.java
+++ main.java
@@ -4,5 +4,6 @@ class yash {
     public static void main(String[] args) {
         System.out.println("Hellp from java project");
         System.out.println(new Date());
+        System.out.println("Welcome again");
     }
 }
