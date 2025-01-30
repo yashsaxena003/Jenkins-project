5b96cc5dff2daa969ec147346f6800ec7bdadb74
Yash Saxena <yashsaxena003@gmail.com>
2025-01-31 00:28:57 +0530

    done

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-class yash {
+class Main {
     public static void main(String[] args) {
         System.out.println("Hellp from java project");
         System.out.println(new Date());
