cde66f96af759172de2ed68a7fc0b5232323ffc4
Yash Saxena <yashsaxena003@gmail.com>
2025-01-31 00:18:02 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class yash {
+    public static void main(String[] args) {
+        System.out.println("Hellp from java project");
+        System.out.println(new Date());
+    }
+}
