dfc59a2f6a17d9bace984aad46d85d8bb489f24b
Yash Saxena <yashsaxena003@gmail.com>
2025-01-31 00:32:38 +0530

    updated

--- main.java
+++ main.java
@@ -2,8 +2,10 @@ import java.util.Date;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hellp from java project");
+        System.out.println("Hello from java project");
         System.out.println(new Date());
         System.out.println("Welcome again");
+        System.out.println("Added new line")
     }
+
 }
